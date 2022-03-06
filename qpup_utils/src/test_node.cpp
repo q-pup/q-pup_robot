60eb7f63ec24caba03bface6f9ca9c9631c53a80
Melvin Wang <melvin.mc.wang@gmail.com>
2022-03-06 14:56:27 -0500

    cleanup test node

--- qpup_utils/src/test_node.cpp
+++ qpup_utils/src/test_node.cpp
@@ -29,28 +29,34 @@ int main(int argc, char** argv) {
     assert(can.writeRTRFrame(QPUP_AXIS_3_GET_VBUS_VOLTAGE_FRAME_ID, sizeof(qpup_odrive_get_vbus_voltage_t)));
     assert(can.writeRTRFrame(QPUP_AXIS_3_HEARTBEAT_FRAME_ID, sizeof(qpup_odrive_heartbeat_t)));
 
-    double motor_error = qpup_odrive_get_motor_error_motor_error_decode(std::get<qpup_odrive_get_motor_error_t>(can.getLatestValue(QPUP_AXIS_3_GET_MOTOR_ERROR_FRAME_ID).value()).motor_error);
-    double vbus_voltage = qpup_odrive_get_vbus_voltage_vbus_voltage_decode(std::get<qpup_odrive_get_vbus_voltage_t>(can.getLatestValue(QPUP_AXIS_3_GET_VBUS_VOLTAGE_FRAME_ID).value()).vbus_voltage);
-//    auto heartbeat_info = std::get<qpup_odrive_heartbeat_t>(can.getLatestValue(QPUP_AXIS_3_HEARTBEAT_FRAME_ID).value());
+    const auto motor_error_opt = can.getLatestValue(QPUP_AXIS_3_GET_MOTOR_ERROR_FRAME_ID);
+    if (motor_error_opt.has_value()) {
+      const uint32_t motor_error = std::get<qpup_odrive_get_motor_error_t>(motor_error_opt.value()).motor_error;
+      ROS_INFO_STREAM("Axis 3 motor error: " << motor_error);
+    }
 
-//    heartbeat_info.axis_error;
-//    heartbeat_info.axis_state;
-//    heartbeat_info.controller_flags;
-//    heartbeat_info.encoder_flags;
-//    heartbeat_info.motor_flags;
+    const auto vbus_opt = can.getLatestValue(QPUP_AXIS_3_GET_VBUS_VOLTAGE_FRAME_ID);
+    if (vbus_opt.has_value()) {
+      const uint32_t vbus_voltage = std::get<qpup_odrive_get_vbus_voltage_t>(vbus_opt.value()).vbus_voltage;
+      ROS_INFO_STREAM("Axis 3 vbus voltage: " << vbus_voltage);
+    }
 
-    ROS_INFO_STREAM("Axis 3 motor error: " << motor_error);
-    ROS_INFO_STREAM("Axis 3 vbus voltage: " << vbus_voltage);
-//    ROS_INFO_STREAM("Axis 3 state: " << hearbeat_info_message.axis_state);
-
-    //  assert(writeFrame(canid_t msg_id, uint8_t* data, uint8_t size);
-  };
+    const auto heartbeat_opt = can.getLatestValue(QPUP_AXIS_3_HEARTBEAT_FRAME_ID);
+    if (heartbeat_opt.has_value()) {
+      const auto heartbeat_info = std::get<qpup_odrive_heartbeat_t>(motor_error_opt.value());
+      ROS_INFO_STREAM("Axis 3 axis_error: " << heartbeat_info.axis_error);
+      ROS_INFO_STREAM("Axis 3 axis_state: " << heartbeat_info.axis_state);
+      ROS_INFO_STREAM("Axis 3 controller_flags: " << heartbeat_info.controller_flags);
+      ROS_INFO_STREAM("Axis 3 encoder_flags: " << heartbeat_info.encoder_flags);
+      ROS_INFO_STREAM("Axis 3 motor_flags: " << heartbeat_info.motor_flags);
+    }
+  }
 
   assert(can.deactivate());
   assert(can.cleanup());
 }
 
-// Encode signals
+// Encode signals (if needed aka if floating or scaled/offset aka noninteger)
 // Pack RTR frame
 // Send Frames
 // get latest RTR
