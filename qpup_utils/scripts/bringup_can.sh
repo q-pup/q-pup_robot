1d5640e3d79682cddcce7d8aec446d13a55e1e2b
Melvin Wang <melvin.mc.wang@gmail.com>
2022-03-06 15:31:03 -0500

    set defualt can0 txqueue length to 10000

--- qpup_utils/scripts/bringup_can.sh
+++ qpup_utils/scripts/bringup_can.sh
@@ -3,13 +3,19 @@
 #TODO: port to python script via subprocess?
 
 if sudo ip link set can0 type can bitrate 1000000; then
-     echo "Successfully setup can interface: can0. Bitrate: 1000000"
+  echo "Successfully set can interface to bitrate 1000000"
 else
-     echo "Failure, exit status: $?"
+  echo "Failure, exit status: $?"
+fi
+
+if sudo ip link set can0 txqueuelen 10000; then
+  echo "Successfully set can interface to txqueuelen 10000"
+else
+  echo "Failure, exit status: $?"
 fi
 
 if sudo ip link set up can0; then
-     echo "Successfully brought up can interface: can0"
+  echo "Successfully brought up can interface: can0"
 else
-     echo "Failure, exit status: $?"
+  echo "Failure, exit status: $?"
 fi
