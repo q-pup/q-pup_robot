8ad27cdf7fab1ae6a241e8458294ea86fb718a04
Melvin Wang <melvin.mc.wang@gmail.com>
2022-03-07 15:43:32 -0500

    fix runtime crash because of deg expression not evaluating when combined with yaml anchors

--- qpup_hw/src/qpup_hw.cpp
+++ qpup_hw/src/qpup_hw.cpp
@@ -33,31 +33,24 @@ bool QPUPHW::init(ros::NodeHandle & /*root_nh*/, ros::NodeHandle &robot_hw_nh) {
   return true;
 }
 
-void QPUPHW::read(const ros::Time & /*time*/,
-                  const ros::Duration & /*period*/) {
-  ROS_ERROR_STREAM_NAMED(name_, "read function called from "
-                                    << name_
-                                    << " class. read calls should only happen "
-                                       "to the overloaded function.");
+void QPUPHW::read(const ros::Time & /*time*/, const ros::Duration & /*period*/) {
+  ROS_ERROR_STREAM_NAMED(name_, "read function called from " << name_
+                                                             << " class. read calls should only happen "
+                                                                "to the overloaded function.");
 }
 
-void QPUPHW::write(const ros::Time & /*time*/,
-                   const ros::Duration & /*period*/) {
-  ROS_ERROR_STREAM_NAMED(name_, "write function called from "
-                                    << name_
-                                    << " class. write calls should only happen "
-                                       "to the overloaded function.");
+void QPUPHW::write(const ros::Time & /*time*/, const ros::Duration & /*period*/) {
+  ROS_ERROR_STREAM_NAMED(name_, "write function called from " << name_
+                                                              << " class. write calls should only happen "
+                                                                 "to the overloaded function.");
 }
 
-void QPUPHW::doSwitch(
-    const std::list<hardware_interface::ControllerInfo> &start_list,
-    const std::list<hardware_interface::ControllerInfo> &stop_list) {
+void QPUPHW::doSwitch(const std::list<hardware_interface::ControllerInfo> &start_list,
+                      const std::list<hardware_interface::ControllerInfo> &stop_list) {
   // Reset commands for joints claimed by stopping controllers
   for (const auto &controller : stop_list) {
-    for (const auto &hardware_interface_resource_list :
-         controller.claimed_resources) {
-      for (const auto &joint_name :
-           hardware_interface_resource_list.resources) {
+    for (const auto &hardware_interface_resource_list : controller.claimed_resources) {
+      for (const auto &joint_name : hardware_interface_resource_list.resources) {
         actuator_joint_commands_[joint_name].type = QPUPActuatorJointCommand::Type::NONE;
         actuator_joint_commands_[joint_name].joint_data = 0.0;
       }
@@ -68,21 +61,14 @@ void QPUPHW::doSwitch(
   for (const auto &controller : start_list) {
     for (const auto &claimed : controller.claimed_resources) {
       for (const auto &joint_name : claimed.resources) {
-        if (claimed.hardware_interface ==
-            "hardware_interface::PositionJointInterface") {
-          actuator_joint_commands_[joint_name].type =
-              QPUPActuatorJointCommand::Type::POSITION;
-          actuator_joint_commands_[joint_name].joint_data =
-              actuator_joint_states_[joint_name].joint_position;
-        } else if (claimed.hardware_interface ==
-                   "hardware_interface::VelocityJointInterface") {
-          actuator_joint_commands_[joint_name].type =
-              QPUPActuatorJointCommand::Type::VELOCITY;
+        if (claimed.hardware_interface == "hardware_interface::PositionJointInterface") {
+          actuator_joint_commands_[joint_name].type = QPUPActuatorJointCommand::Type::POSITION;
+          actuator_joint_commands_[joint_name].joint_data = actuator_joint_states_[joint_name].joint_position;
+        } else if (claimed.hardware_interface == "hardware_interface::VelocityJointInterface") {
+          actuator_joint_commands_[joint_name].type = QPUPActuatorJointCommand::Type::VELOCITY;
           actuator_joint_commands_[joint_name].joint_data = 0.0;
-        } else if (claimed.hardware_interface ==
-                   "hardware_interface::EffortJointInterface") {
-          actuator_joint_commands_[joint_name].type =
-              QPUPActuatorJointCommand::Type::EFFORT;
+        } else if (claimed.hardware_interface == "hardware_interface::EffortJointInterface") {
+          actuator_joint_commands_[joint_name].type = QPUPActuatorJointCommand::Type::EFFORT;
           actuator_joint_commands_[joint_name].joint_data = 0.0;
         }
       }
@@ -118,96 +104,72 @@ bool QPUPHW::loadJointInfoFromParameterServer(ros::NodeHandle &robot_hw_nh) {
   return true;
 }
 
-void QPUPHW::registerStateInterfacesAndTransmissions(
-    const std::string &joint_name) {
+void QPUPHW::registerStateInterfacesAndTransmissions(const std::string &joint_name) {
   // Register JointStateHandle to the JointStateInterface
   hardware_interface::JointStateHandle joint_state_handle(
       joint_name, &actuator_joint_states_[joint_name].joint_position,
-      &actuator_joint_states_[joint_name].joint_velocity,
-      &actuator_joint_states_[joint_name].joint_effort);
+      &actuator_joint_states_[joint_name].joint_velocity, &actuator_joint_states_[joint_name].joint_effort);
   joint_state_interface_.registerHandle(joint_state_handle);
 
   // Wrap Actuators States
-  actuator_state_data_[joint_name].position.push_back(
-      &actuator_joint_states_[joint_name].actuator_position);
-  actuator_state_data_[joint_name].velocity.push_back(
-      &actuator_joint_states_[joint_name].actuator_velocity);
-  actuator_state_data_[joint_name].effort.push_back(
-      &actuator_joint_states_[joint_name].actuator_effort);
+  actuator_state_data_[joint_name].position.push_back(&actuator_joint_states_[joint_name].actuator_position);
+  actuator_state_data_[joint_name].velocity.push_back(&actuator_joint_states_[joint_name].actuator_velocity);
+  actuator_state_data_[joint_name].effort.push_back(&actuator_joint_states_[joint_name].actuator_effort);
 
   // Wrap Joint States
-  joint_state_data_[joint_name].position.push_back(
-      &actuator_joint_states_[joint_name].joint_position);
-  joint_state_data_[joint_name].velocity.push_back(
-      &actuator_joint_states_[joint_name].joint_velocity);
-  joint_state_data_[joint_name].effort.push_back(
-      &actuator_joint_states_[joint_name].joint_effort);
+  joint_state_data_[joint_name].position.push_back(&actuator_joint_states_[joint_name].joint_position);
+  joint_state_data_[joint_name].velocity.push_back(&actuator_joint_states_[joint_name].joint_velocity);
+  joint_state_data_[joint_name].effort.push_back(&actuator_joint_states_[joint_name].joint_effort);
 
   // Register ActuatorToJointStateHandle with wrapped state data to a
   // ActuatorToJointStateInterface
-  transmission_interface::ActuatorToJointStateHandle
-      actuator_to_joint_state_handle(
-          joint_name, &joint_transmissions_.find(joint_name)->second,
-          actuator_state_data_[joint_name], joint_state_data_[joint_name]);
-  actuator_to_joint_state_interface_.registerHandle(
-      actuator_to_joint_state_handle);
+  transmission_interface::ActuatorToJointStateHandle actuator_to_joint_state_handle(
+      joint_name, &joint_transmissions_.find(joint_name)->second, actuator_state_data_[joint_name],
+      joint_state_data_[joint_name]);
+  actuator_to_joint_state_interface_.registerHandle(actuator_to_joint_state_handle);
 
   // TODO: register new state interface for motor voltage, current, etc.
 }
 
-void QPUPHW::registerCommandInterfacesAndTransmissions(
-    const std::string &joint_name) {
+void QPUPHW::registerCommandInterfacesAndTransmissions(const std::string &joint_name) {
   // Register JointHandle associated with a JointStateHandle to command
   // interfaces
-  hardware_interface::JointHandle joint_command_handle(
-      joint_state_interface_.getHandle(joint_name),
-      &actuator_joint_commands_[joint_name].joint_data);
+  hardware_interface::JointHandle joint_command_handle(joint_state_interface_.getHandle(joint_name),
+                                                       &actuator_joint_commands_[joint_name].joint_data);
   joint_position_interface_.registerHandle(joint_command_handle);
   joint_velocity_interface_.registerHandle(joint_command_handle);
   joint_effort_interface_.registerHandle(joint_command_handle);
 
   // Wrap Actuator Commands
-  actuator_command_data_[joint_name].position.push_back(
-      &actuator_joint_commands_[joint_name].actuator_data);
-  actuator_command_data_[joint_name].velocity.push_back(
-      &actuator_joint_commands_[joint_name].actuator_data);
-  actuator_command_data_[joint_name].effort.push_back(
-      &actuator_joint_commands_[joint_name].actuator_data);
+  actuator_command_data_[joint_name].position.push_back(&actuator_joint_commands_[joint_name].actuator_data);
+  actuator_command_data_[joint_name].velocity.push_back(&actuator_joint_commands_[joint_name].actuator_data);
+  actuator_command_data_[joint_name].effort.push_back(&actuator_joint_commands_[joint_name].actuator_data);
 
   // Wrap Joint Commands
-  joint_command_data_[joint_name].position.push_back(
-      &actuator_joint_commands_[joint_name].joint_data);
-  joint_command_data_[joint_name].velocity.push_back(
-      &actuator_joint_commands_[joint_name].joint_data);
-  joint_command_data_[joint_name].effort.push_back(
-      &actuator_joint_commands_[joint_name].joint_data);
+  joint_command_data_[joint_name].position.push_back(&actuator_joint_commands_[joint_name].joint_data);
+  joint_command_data_[joint_name].velocity.push_back(&actuator_joint_commands_[joint_name].joint_data);
+  joint_command_data_[joint_name].effort.push_back(&actuator_joint_commands_[joint_name].joint_data);
 
   // Register JointToActuatorPositionHandle with wrapped command data to a
   // JointToActuatorPositionInterface
-  transmission_interface::JointToActuatorPositionHandle
-      joint_to_actuator_position_handle(
-          joint_name, &joint_transmissions_.find(joint_name)->second,
-          actuator_command_data_[joint_name], joint_command_data_[joint_name]);
-  joint_to_actuator_position_interface_.registerHandle(
-      joint_to_actuator_position_handle);
+  transmission_interface::JointToActuatorPositionHandle joint_to_actuator_position_handle(
+      joint_name, &joint_transmissions_.find(joint_name)->second, actuator_command_data_[joint_name],
+      joint_command_data_[joint_name]);
+  joint_to_actuator_position_interface_.registerHandle(joint_to_actuator_position_handle);
 
   // Register JointToActuatorVelocityHandle with wrapped command data to a
   // JointToActuatorVelocityInterface
-  transmission_interface::JointToActuatorVelocityHandle
-      joint_to_actuator_velocity_handle(
-          joint_name, &joint_transmissions_.find(joint_name)->second,
-          actuator_command_data_[joint_name], joint_command_data_[joint_name]);
-  joint_to_actuator_velocity_interface_.registerHandle(
-      joint_to_actuator_velocity_handle);
+  transmission_interface::JointToActuatorVelocityHandle joint_to_actuator_velocity_handle(
+      joint_name, &joint_transmissions_.find(joint_name)->second, actuator_command_data_[joint_name],
+      joint_command_data_[joint_name]);
+  joint_to_actuator_velocity_interface_.registerHandle(joint_to_actuator_velocity_handle);
 
   // Register JointToActuatorEffortHandle with wrapped command data to a
   // JointToActuatorVelocityInterface
-  transmission_interface::JointToActuatorEffortHandle
-      joint_to_actuator_effort_handle(
-          joint_name, &joint_transmissions_.find(joint_name)->second,
-          actuator_command_data_[joint_name], joint_command_data_[joint_name]);
-  joint_to_actuator_effort_interface_.registerHandle(
-      joint_to_actuator_effort_handle);
+  transmission_interface::JointToActuatorEffortHandle joint_to_actuator_effort_handle(
+      joint_name, &joint_transmissions_.find(joint_name)->second, actuator_command_data_[joint_name],
+      joint_command_data_[joint_name]);
+  joint_to_actuator_effort_interface_.registerHandle(joint_to_actuator_effort_handle);
 }
 
 }  // namespace qpup_hw
